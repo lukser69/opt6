0ab6deeac087e19c8ba51eae8a35e4f5b6e9454e
lukser69 <russergey2001@mail.ru>
2024-05-18 17:50:35 +0300

    feat: add layout

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+	<section class="default-layout">
+		<main>
+			<slot />
+		</main>
+	</section>
+</template>
+
+<style scoped>
+.default-layout {
+	min-height: 100dvh;
+}
+</style>
